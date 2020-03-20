c2cc4789c4a2aba1be9304343533730fe451582a
SEM3708 <SEM3708@10.0.0.38>
2020-03-19 22:39:22 -0400

    Hello world 2

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Steven Mason is here");
+		System.out.println("Steven Mason is NOT here");
+		
+		System.out.println("Hi here");
 	}
 }
